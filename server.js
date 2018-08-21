470cb23d2ae08d19b5b5e54c872d4ccf83512673
Sean Thomas <sathomas100@gmail.com>
2018-08-21 14:55:38 -0600

    fixes to cas authentication

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cas = require('byu-cas');
+var session = require('express-session');
+var CASAuthentication = require('cas-authentication');
 var app = express();
 
+
+app.use( session({
+    secret            : 'asfjip2q3r__u89fwdiojfDSJFDSJ**FF',
+    resave            : false,
+    saveUninitialized : true
+}));
+
+// Create a new instance of CASAuthentication.
+var cas = new CASAuthentication({
+    cas_url     : 'https://cas.byu.edu/cas/',
+    service_url : 'http://localhost:4000',
+    cas_version: 'saml1.1'
+});
 
 app.use(bodyParser.json());
 /*app.use((req, res, next) => {
@@ -11,33 +26,34 @@ app.use(bodyParser.json());
     next();
 })*/
 
-function doSomethingToGetTicket(){
-	console.log('ticket');
-}
-
-var ticket = doSomethingToGetTicket();
-var service = 'http://localhost:4000/';
-
-/*app.get('/', function(req, res){
-	res.sendStatus(200);
-})*/
-
 var api = express.Router();
 
-api.get('/', (req, res)=>{
-	res.sendStatus(200);
+api.get('/', cas.bounce, (req, res)=>{
+	res.send( '<html><body>Hello!</body></html>' );
 });
 
-api.get('/signin', (req, res)=>{
-	cas.validate(ticket, service).then(function success(response) {
-	  console.log("Ticket valid! Hello, " + response.username);
-	  console.dir(response.attributes);
-    res.sendStatus(200);
-	}).catch(function error(e) {
-		res.sendStatus(401);
-	  console.log("Invalid ticket. Error message was: " + e.message);
-	});
+
+// Unauthenticated clients will receive a 401 Unauthorized response instead of
+// the JSON data.
+api.get( '/api', cas.block, ( req, res ) => {
+    res.json( { success: true } );
 });
+
+// An example of accessing the CAS user session variable. This could be used to
+// retrieve your own local user records based on authenticated CAS username.
+api.get( '/api/user', cas.block, ( req, res ) => {
+    res.json( { cas_user: req.session[ cas.session_name ] } );
+});
+
+// Unauthenticated clients will be redirected to the CAS login and then to the
+// provided "redirectTo" query parameter once authenticated.
+api.get( '/authenticate', cas.bounce_redirect );
+ 
+// This route will de-authenticate the client with the Express server and then
+// redirect the client to the CAS logout page.
+api.get( '/logout', cas.logout, (req, res)=>{
+	res.send( '<html><body>Log Out Successful!</body></html>' );
+} );
 
 app.use('/', api);
 
